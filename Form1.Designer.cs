bbecfbde78dcd307378635c5f7c0d4953e1e95c5
Nursultan Kassan <kassan.nk@outlook.com>
2023-05-25 11:41:40 +0600

    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WorkOrderTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtSearchTX = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.testDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.testDataSet = new WorkOrderTracker.testDataSet();
+            this.WorkOrder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Entry = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearchTX
+            // 
+            this.txtSearchTX.Location = new System.Drawing.Point(57, 28);
+            this.txtSearchTX.Name = "txtSearchTX";
+            this.txtSearchTX.Size = new System.Drawing.Size(100, 20);
+            this.txtSearchTX.TabIndex = 0;
+            this.txtSearchTX.Text = "Search TX# ";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(222, 28);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.WorkOrder,
+            this.Entry,
+            this.Description,
+            this.Amount});
+            this.dataGridView1.DataSource = this.testDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(57, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(448, 238);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // testDataSetBindingSource
+            // 
+            this.testDataSetBindingSource.DataSource = this.testDataSet;
+            this.testDataSetBindingSource.Position = 0;
+            // 
+            // testDataSet
+            // 
+            this.testDataSet.DataSetName = "testDataSet";
+            this.testDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // WorkOrder
+            // 
+            this.WorkOrder.DataPropertyName = "t.WorkOrderID";
+            this.WorkOrder.HeaderText = "WorkOrder";
+            this.WorkOrder.Name = "WorkOrder";
+            // 
+            // Entry
+            // 
+            this.Entry.HeaderText = "Entry";
+            this.Entry.Name = "Entry";
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "Description";
+            this.Description.Name = "Description";
+            // 
+            // Amount
+            // 
+            this.Amount.HeaderText = "Amount";
+            this.Amount.Name = "Amount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchTX);
+            this.Name = "Form1";
+            this.Text = "All related transactions associated with tx number";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearchTX;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource testDataSetBindingSource;
+        private testDataSet testDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn WorkOrder;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Entry;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkOrderTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Get the transaction entry number from the textbox.
+            int entryNumber = int.Parse(txtSearchTX.Text);
+
+            // Connect to the database.
+            string connectionString = "Data Source=localhost;Initial Catalog=WorkOrderTracker;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Execute a query to get the work order details.
+                string query = @"
+            SELECT
+                w.Name,
+                w.Description,
+                t.EntryNumber,
+                t.Description,
+                ti.ItemName,
+                ti.Quantity,
+                ti.UnitPrice,
+                ti.Total
+            FROM dbo.WorkOrder w
+            INNER JOIN dbo.WOTransaction t ON w.ID = t.WorkOrderID
+            INNER JOIN dbo.WOTransactionItem ti ON t.ID = ti.WOTransactionID
+            WHERE t.EntryNumber = @entryNumber;
+        ";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@entryNumber", entryNumber);
+
+                // Execute the query.
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Display the results in a grid.
+                GridView gridView = new GridView();
+                gridView.Dock = DockStyle.Fill;
+                gridView.DataSource = reader;
+
+                // Add the grid view to the form.
+                this.Controls.Add(gridView);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
